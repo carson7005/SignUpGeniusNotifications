27e39cc6018c76b184a9776a7307aaea3bbfeebd
KCsup <kcsup05@gmail.com>
2023-02-20 01:39:16 -0500

    Optimize imports for main script.

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import signup_util as sutil
-import canvas_util as cutil
-import google_calendar_util as gcalutil
 import notif_util as nutil
-import argparse
-from datetime import date, timedelta
 
 
 def main():
