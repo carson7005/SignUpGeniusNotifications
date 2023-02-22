6056d8c84100d36489033a3b48999ddec715b9f1
KCsup <kcsup05@gmail.com>
2023-02-22 17:58:37 +0000

    Updated job scheduling.

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import notif_util as nutil
 import schedule
 import time
 
+def hourly_job(t):
+    nutil.send_hourly_notification(2, True)
+    print("Hourly job done.")
+
+def daily_job(t):
+    nutil.send_daily_notification(7)
+    print("Daily jon done.")
 
 def main():
-    schedule.every().hour.at(":05").do(
-            nutil.send_hourly_notification(2, True))
-    schedule.every().monday.at("8:00").do(
-            nutil.send_daily_notification(7))
+    schedule.every().hour.at(":05").do(hourly_job)
+    schedule.every().monday.at("08:00").do(daily_job)
 
     while True:
         schedule.run_pending()
