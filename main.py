1db738c944889e3f55fe189f95c1129ae0b56b0f
KCsup <kcsup05@gmail.com>
2023-02-22 12:19:11 -0500

    Added basic scheduling, moving to test deployment.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import notif_util as nutil
+import schedule
+import time
 
 
 def main():
-    #nutil.send_weekly_notification()
-    nutil.send_hourly_notification(2, True)
+    schedule.every().hour.do(
+            nutil.send_hourly_notification(2, True))
+    schedule.every().monday.at("8:00").do(
+            nutil.send_daily_notification(7))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
 
 
 if __name__ == "__main__":
