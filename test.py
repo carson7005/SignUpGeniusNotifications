c6f4f8f10d0b7d523274ddaef8cfb84a0240dc62
KCsup <kcsup05@gmail.com>
2023-03-01 00:46:40 -0500

    Update test file.

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import time
 
 def main():
     start = time.time()
-    nutil.send_daily_notification(7)
+    nutil.send_hourly_notification(2, 1, include_when=True)
+    # nutil.send_daily_notification(7)
 
     uptime = time.time() - start
-    print(f"Uptime: {uptime})
+    print(f"Uptime: {uptime}")
 
 
 if __name__ == "__main__":
