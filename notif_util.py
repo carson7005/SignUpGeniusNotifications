24d94564a6b5bbbe3a32a1f365326fa523d0db64
KCsup <kcsup05@gmail.com>
2023-03-13 00:01:31 -0400

    Remove unused notif methods, add dedicated weekly notif method.

--- notif_util.py
+++ notif_util.py
@@ -61,14 +61,18 @@ def get_notification_message(days_out=None,
     return notif_message, len(signups_notify)
 
 
-def get_signups_to_notify(days_out=None, days_from=0, hours_out=None, hours_from=0):
+def get_signups_to_notify(days_out=None,
+                          days_from=0,
+                          hours_out=None,
+                          hours_from=0,
+                          retries=5):
     if not days_out and not hours_out:
         return None
 
     signups = []
     links = link_util.get_current_links()
     for l in links:
-        signup = sutil.get_signup_data(l, 5)
+        signup = sutil.get_signup_data(l, retries)
         roles = signup.get_roles(days_out=days_out,
                                  days_from=days_from,
                                  hours_out=hours_out,
@@ -81,44 +85,6 @@ def get_signups_to_notify(days_out=None, days_from=0, hours_out=None, hours_from
     return signups
 
 
-
-def get_signups_to_notify_calendar(days_out, days_from):
-    signups = []
-    events = gcalutil.get_nhs_events()
-    for e in events:
-        signup_link = gcalutil.get_signup_link_from_event(e)
-
-        if signup_link == None: continue
-
-        signup = sutil.get_signup_data(signup_link, 5)
-        roles = signup.get_roles(days_out=days_out, days_from=days_from)
-
-        if not roles: continue
-
-        signups.append(signup)
-
-    return signups
-
-
-def get_signups_to_notify_hourly_calendar(hours_out, hours_from):
-    signups = []
-    events = gcalutil.get_nhs_events()
-    for e in events:
-        signup_link = gcalutil.get_signup_link_from_event(e)
-
-        if signup_link == None: continue
-
-        signup = sutil.get_signup_data(signup_link, 5)
-        roles = signup.get_roles(hours_out=hours_out, hours_from=hours_from)
-
-        if not roles: continue
-
-        signups.append(signup)
-
-    return signups
-
-
-
 def send_daily_notification(days_out, days_from=0, include_when=False):
     notif_message, signup_count = get_notification_message(days_out=days_out,
                                                            days_from=days_from,
@@ -126,6 +92,22 @@ def send_daily_notification(days_out, days_from=0, include_when=False):
 
     if signup_count == 0:
         lutil.log(f"No signups for daily update ({days_out} days), skipping.")
+        return
+
+    current_date_str = date.today().strftime("%m/%d/%Y")
+    notif_title = f"Daily Update for SignUps ({current_date_str})"
+    notif_message = notif_title + "<br><br>" + notif_message
+    default_course = cutil.get_notification_course_id()
+    cutil.send_announcement(default_course, notif_title, notif_message)
+
+
+def send_weekly_notification(days_out=7, days_from=0, include_when=False):
+    notif_message, signup_count = get_notification_message(days_out=days_out,
+                                                           days_from=days_from,
+                                                           include_when=include_when)
+
+    if signup_count == 0:
+        lutil.log(f"No signups for weekly update ({days_out} days), skipping.")
         return
 
     current_date_str = date.today().strftime("%m/%d/%Y")
