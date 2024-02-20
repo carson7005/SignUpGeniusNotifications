fee4fb87a1d55b621075de19dfd36282c8ca010d
carson7005 <152827386+carson7005@users.noreply.github.com>
2024-02-20 10:44:55 -0500

    Commenting config_util.py

--- util/config_util.py
+++ util/config_util.py
@@ -1,6 +1,6 @@
-import json
+import json #imports the library to access JSON dictionaries
 
-def get_config():
+def get_config(): #Function to open and load the file into an object named data; which is then returned
     config_file = open("config.json")
     data = json.load(config_file)
     config_file.close()
@@ -8,5 +8,5 @@ def get_config():
     return data
 
 
-def get_config_item(key):
+def get_config_item(key): #Given a key, the function returns the value from the dictionary given a key
     return get_config()[key]
